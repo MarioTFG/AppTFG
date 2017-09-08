5d7a19c502909762e27e5ff497a4a01068bf2fba
MarioTFG <delmazo.mario@gmail.com>
2017-09-08 11:14:34 +0200

    primer commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appTfgApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appTfgApp
+ */
+angular.module('appTfgApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appTfgApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appTfgApp
+ */
+angular.module('appTfgApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
